d95be9ba669d73414c30448ceaac5b34a0d1982e
JuanCardona17 <146267439+JuanCardona17@users.noreply.github.com>
2024-11-17 00:19:03 -0500

    ServidorReporte hecho para pruebas de consola

---
+++ src/main/java/co/edu/uniquindio/proyectofinal/clases/ClienteReporte.java
@@ -0,0 +1,35 @@
+package co.edu.uniquindio.proyectofinal.clases;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class ClienteReporte {
+
+    public static void main (String [] args) throws IOException{
+        try (Socket socket = new Socket ("LocalHost", 12345);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+
+            Scanner scanner = new Scanner (System.in);
+            System.out.println("Ingrese la fecha en el formato yyyy-mm-dd: ");
+            String fechaInput = scanner.nextLine();
+
+            LocalDate fecha = LocalDate.parse(fechaInput, DateTimeFormatter.ISO_LOCAL_DATE);
+
+            out.writeObject(fecha);
+            out.flush();
+
+            String reporte;
+            System.out.println("Reporte recibido");
+            while ((reporte = in.readLine()) != null){
+            System.out.println(reporte);}
+        }
+    }
+
+}
