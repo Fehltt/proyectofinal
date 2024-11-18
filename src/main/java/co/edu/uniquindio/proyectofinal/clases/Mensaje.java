24fb628a1a027c9f591cc25d9bb44e67042ca68f
Juans3bitas <juansebitas729@gmail.com>
2024-11-17 22:52:57 -0500

    Modificación en Producto, Mensaje, Comentario y Muro

--- src/main/java/co/edu/uniquindio/proyectofinal/clases/Mensaje.java
+++ src/main/java/co/edu/uniquindio/proyectofinal/clases/Mensaje.java
@@ -2,17 +2,15 @@ package co.edu.uniquindio.proyectofinal.clases;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.logging.Level;
+
+
 
 public class Mensaje implements Serializable {
     
     private Vendedor remitente;
     private Vendedor destinatario;
     private String contenido;
-    private LocalDateTime fechaHora; 
-    private List<Comentario> comentarios; 
-    private List<Vendedor> meGustas;
+    private LocalDateTime fechaHora;  
 
     // Constructor
     public Mensaje(Vendedor remitente, Vendedor destinatario, String contenido) {
@@ -52,42 +50,6 @@ public class Mensaje implements Serializable {
 
     public void setFechaHora(LocalDateTime fechaHora) {
         this.fechaHora = fechaHora;
-    }
-
-    public List<Comentario> getComentarios() {
-        return comentarios;
-    }
-
-    public void setComentarios(List<Comentario> comentarios) {
-        this.comentarios = comentarios;
-    }
-
-    public List<Vendedor> getMeGustas() {
-        return meGustas;
-    }
-
-    public void setMeGustas(List<Vendedor> meGustas) {
-        this.meGustas = meGustas;
-    }
-
-    //Métodos para comentarios
-    public void agregarComentario(Comentario comentario) { 
-        comentarios.add(comentario);
-        Utilidades.getInstance().escribirLog(Level.INFO, "Función agregarComentario en Mensaje. Funcionamiento adecuado");
-    } 
-    public void eliminarComentario(Comentario comentario) { 
-        comentarios.remove(comentario);
-        Utilidades.getInstance().escribirLog(Level.INFO, "Función eliminarComentario en Mensaje. Funcionamiento adecuado");
-    } 
-
-    // Métodos para "me gusta" 
-    public void darMeGusta(Vendedor vendedor) { 
-        meGustas.add(vendedor);
-        Utilidades.getInstance().escribirLog(Level.INFO, "Función darMeGusta en Mensaje. Funcionamiento adecuado");
-    } 
-    public void quitarMeGusta(Vendedor vendedor) { 
-        meGustas.remove(vendedor);
-        Utilidades.getInstance().escribirLog(Level.INFO, "Función quitarMeGusta en Mensaje. Funcionamiento adecuado");
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module co.edu.uniquindio.proyectofinal {
     requires java.logging;
     requires transitive javafx.graphics;
     requires java.xml;
+    requires java.desktop;
     
     exports co.edu.uniquindio.proyectofinal.clases;
     opens co.edu.uniquindio.proyectofinal.controllers to javafx.fxml;
